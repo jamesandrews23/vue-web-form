18e6420bd97dd26af33c4d964c31ec1bfa874cf2
jamesandrews23 <james.andrews23@gmail.com>
2021-08-31 16:45:48 -0500

    Adds the submit event with the prevent modifier to the form and logs the data.

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,9 +1,10 @@
 <template>
-    <form>
+    <form @submit.prevent="handleSubmit">
         <label>Email:</label>
         <input type="email" required v-model="email" />
         <label>Password:</label>
         <input type="password" required v-model="password" />
+        <div v-if="passwordError" class="error">{{ passwordError }}</div>
         <label>
             Role:
         </label>
@@ -40,6 +41,10 @@
 <!--            <input type="checkbox" v-model="names" value="lugai" />-->
 <!--            <label>Lugai</label>-->
 <!--        </div>-->
+
+      <div class="submit">
+        <button>Create</button>
+      </div>
     </form>
     <p>Email: {{ email }}</p>
     <p>password: {{ password }}</p>
@@ -59,7 +64,8 @@
                 terms: false,
                 // names: []
                 tempSkill: '',
-                skills: []
+                skills: [],
+                passwordError: ''
             }
         },
         methods:{
@@ -73,6 +79,18 @@
             },
             removeSkill(skill){
                 this.skills = this.skills.filter(item => { return skill !== item });
+            },
+            handleSubmit(){
+                // validate pass
+                this.passwordError = this.password.length > 5 ? '' : 'Password must be at least 6 chars long';
+
+                if(!this.passwordError){
+                  console.log('email: ', this.email);
+                  console.log('password: ', this.password);
+                  console.log('role: ', this.role);
+                  console.log('skills: ', this.skills);
+                  console.log('terms accepted: ', this.terms);
+                }
             }
         }
     }
@@ -123,5 +141,22 @@
         font-weight: bold;
         color: #777;
         cursor: pointer;
+    }
+    button {
+      background: #0b6dff;
+      border: 0;
+      padding: 10px 20px;
+      margin-top: 20px;
+      color: white;
+      border-radius:20px;
+    }
+    .submit {
+      text-align:center;
+    }
+    .error {
+      color: #ff0062;
+      margin-top:10px;
+      font-size:0.8em;
+      font-weight:bold;
     }
 </style>
